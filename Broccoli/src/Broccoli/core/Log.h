b11231fd3e12a2a71c70e0fbc3e6e944da4703ee
Trent Ross <tross2552@gmail.com>
2021-07-07 11:51:50 -0400

    Fix wrong spdlog api call.

--- Broccoli/src/Broccoli/core/Log.h
+++ Broccoli/src/Broccoli/core/Log.h
@@ -34,15 +34,15 @@ namespace brcl
 }
 
 //Core Log Macros
-#define BRCL_CORE_TRACE(...)   ::brcl::Log::GetCoreLogger()->trace(__VA_ARGS__)
-#define BRCL_CORE_INFO(...)    ::brcl::Log::GetCoreLogger()->info(__VA_ARGS__)
-#define BRCL_CORE_WARN(...)    ::brcl::Log::GetCoreLogger()->warn(__VA_ARGS__)
-#define BRCL_CORE_ERROR(...)   ::brcl::Log::GetCoreLogger()->error(__VA_ARGS__)
-#define BRCL_CORE_FATAL(...)   ::brcl::Log::GetCoreLogger()->fatal(__VA_ARGS__)
+#define BRCL_CORE_TRACE(...)      ::brcl::Log::GetCoreLogger()->trace(__VA_ARGS__)
+#define BRCL_CORE_INFO(...)       ::brcl::Log::GetCoreLogger()->info(__VA_ARGS__)
+#define BRCL_CORE_WARN(...)       ::brcl::Log::GetCoreLogger()->warn(__VA_ARGS__)
+#define BRCL_CORE_ERROR(...)      ::brcl::Log::GetCoreLogger()->error(__VA_ARGS__)
+#define BRCL_CORE_CRITICAL(...)   ::brcl::Log::GetCoreLogger()->critical(__VA_ARGS__)
 
 //Client Log Macros
-#define BRCL_TRACE(...)        ::brcl::Log::GetClientLogger()->trace(__VA_ARGS__)
-#define BRCL_INFO(...)         ::brcl::Log::GetClientLogger()->info(__VA_ARGS__)
-#define BRCL_WARN(...)         ::brcl::Log::GetClientLogger()->warn(__VA_ARGS__)
-#define BRCL_ERROR(...)        ::brcl::Log::GetClientLogger()->error(__VA_ARGS__)
-#define BRCL_FATAL(...)        ::brcl::Log::GetClientLogger()->fatal(__VA_ARGS__)
+#define BRCL_TRACE(...)           ::brcl::Log::GetClientLogger()->trace(__VA_ARGS__)
+#define BRCL_INFO(...)            ::brcl::Log::GetClientLogger()->info(__VA_ARGS__)
+#define BRCL_WARN(...)            ::brcl::Log::GetClientLogger()->warn(__VA_ARGS__)
+#define BRCL_ERROR(...)           ::brcl::Log::GetClientLogger()->error(__VA_ARGS__)
+#define BRCL_CRITICAL(...)        ::brcl::Log::GetClientLogger()->critical(__VA_ARGS__)
