45b3c05fd2152bf24744a841399e3068fd425070
Trent Ross <tross2552@gmail.com>
2021-07-08 19:33:33 -0400

    Implement reverse iterators for LayerStack.

--- Broccoli/src/Broccoli/LayerStack.h
+++ Broccoli/src/Broccoli/LayerStack.h
@@ -1,7 +1,7 @@
 #pragma once
 
 #include "Layer.h"
-
+#include <memory>
 #include <vector>
 
 namespace brcl
@@ -9,7 +9,7 @@ namespace brcl
 	class BRCL_API LayerStack
 	{
 	public:
-		//essentially a wrapper for a Layer* vector. overlays in first half, layers in second half
+		
 		LayerStack();
 		~LayerStack();
 
@@ -19,8 +19,15 @@ namespace brcl
 		void PushOverlay(Layer* layer);
 		void PopOverlay(Layer* layer);
 
-		inline std::vector<Layer*>::iterator begin() { return m_Layers.begin(); }
-		inline std::vector<Layer*>::iterator end() { return m_Layers.end(); }
+		std::vector<Layer*>::iterator         begin() { return m_Layers.begin(); }
+		std::vector<Layer*>::iterator         end() { return m_Layers.end(); }
+		std::vector<Layer*>::reverse_iterator rbegin() { return m_Layers.rbegin(); }
+		std::vector<Layer*>::reverse_iterator rend() { return m_Layers.rend(); }
+
+		std::vector<Layer*>::const_iterator         begin()           const { return m_Layers.begin(); }
+		std::vector<Layer*>::const_iterator         end()	          const { return m_Layers.end(); }
+		std::vector<Layer*>::const_reverse_iterator rbegin()  const { return m_Layers.rbegin(); }
+		std::vector<Layer*>::const_reverse_iterator rend()    const { return m_Layers.rend(); }
 
 
 	private:
