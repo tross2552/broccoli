e735e4252423d93fe4a0864c24e6f33248a9d17b
Trent Ross <tross2552@gmail.com>
2021-07-19 14:23:23 -0400

    Add debug assertions to entity class.

--- Broccoli/src/Broccoli/Scene/Entity.h
+++ Broccoli/src/Broccoli/Scene/Entity.h
@@ -20,24 +20,31 @@ namespace brcl
 		template <typename T, typename... Args>
 		T& AddComponent(Args &&... args)
 		{
+			BRCL_CORE_ASSERT(m_Scene->m_Registry.valid(m_Handle), "Scene error: Entity not valid!");
+			BRCL_CORE_ASSERT(m_Scene->m_Registry.all_of<T>(m_Handle), "Scene error: Entity already has component!");
 			return m_Scene->m_Registry.emplace<T>(m_Handle, std::forward<Args>(args)...);
 		}
 
 		template <typename T>
 		T* GetComponent()
 		{
-			return m_Scene->m_Registry.try_get<T>(m_Handle);
+			BRCL_CORE_ASSERT(m_Scene->m_Registry.valid(m_Handle), "Scene error: Entity not valid!");
+			BRCL_CORE_ASSERT(m_Scene->m_Registry.all_of<T>(m_Handle), "Scene error: Entity does not have component!");
+			return m_Scene->m_Registry.get<T>(m_Handle);
 		}
 
 		template <typename T>
 		bool HasComponent()
 		{
+			BRCL_CORE_ASSERT(m_Scene->m_Registry.valid(m_Handle), "Scene error: Entity not valid!");
 			return m_Scene->m_Registry.any_of<T>(m_Handle);
 		}
 
 		template <typename T>
 		void RemoveComponent()
 		{
+			BRCL_CORE_ASSERT(m_Scene->m_Registry.valid(m_Handle), "Scene error: Entity not valid!");
+			BRCL_CORE_ASSERT(m_Scene->m_Registry.all_of<T>(m_Handle), "Scene error: Entity does not have component!");
 			m_Scene->m_Registry.remove_if_exists<T>(m_Handle);
 		}
 
